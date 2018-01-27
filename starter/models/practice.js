2243d69e190fe3ea4e288938fcab86d9c3fcbe4e
mtclark0518 <mtclark0518@gmail.com>
2018-01-26 23:56:27 -0700

    removes code from master branch

--- starter/models/practice.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var Practice = mongoose.Schema({
-    first        : String,
-    second       : Number,
-});
-
-
-module.exports = mongoose.model('Practice', Practice);
